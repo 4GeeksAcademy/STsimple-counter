a1b53c19a245890581155b7f5ea9037e91868517
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-06-07 19:33:09 +0000

    actualizadoST

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState, useEffect } from 'react';
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  const [segundos, setSegundos] = useState(0);
+	
+	  useEffect(() => {
+		// Configurar el temporizador para incrementar el contador cada segundo
+		const intervalo = setInterval(() => {
+		  setSegundos((prevSegundos) => prevSegundos + 1);
+		}, 1000);
+	
+		// Limpiar el temporizador cuando el componente se desmonte
+		return () => clearInterval(intervalo);
+	  }, []); // El array vacío asegura que este efecto se ejecute solo una vez cuando el componente se monta
+	
+	  return (
+		<div>
+		  <h1>Contador de Segundos</h1>
+		  <p>Segundos: {segundos}</p>
 		</div>
-	);
-};
-
+	  );
+	};
 export default Home;
