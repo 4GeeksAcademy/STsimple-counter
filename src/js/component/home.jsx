dc42e654c124c563519e2c2881999faa9f17a20e
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-06-11 16:49:09 +0000

    STend

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,20 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faClock} from '@fortawesome/free-solid-svg-icons';
 //create your first component
 const Home = () => {
-	  const [segundos, setSegundos] = useState(0);
-	
-	  useEffect(() => {
-		// Configurar el temporizador para incrementar el contador cada segundo
+	const [segundos, setSegundos] = useState(0);
+	const [isRunning, setIsRunning] = useState(true);
+	  
+	const resetCounter = () => {
+        setSegundos(0);
+    	};
+
+	const stopCounter = () => {
+		setIsRunning(false); 
+	}
+
+	const resumeCounter = () => {
+		setIsRunning(true); 
+	}
+		
+	useEffect(() => {
+		if (!isRunning) return;
+		
 		const intervalo = setInterval(() => {
 		  setSegundos((prevSegundos) => prevSegundos + 1);
 		}, 1000);
-	
-		// Limpiar el temporizador cuando el componente se desmonte
+		
 		return () => clearInterval(intervalo);
-	  	}, []); // El array vacío asegura que este efecto se ejecute solo una vez cuando el componente se monta
+	  	}, [isRunning]); 
 	
 		const formatSeconds = (num) => {
-			return String(num).padStart(6, '0'); // Asegura que siempre haya 6 dígitos (hh:mm:ss)
+			return String(num).padStart(6, '0'); 
 		  };
 		
 		  const digits = formatSeconds(segundos).split('');
@@ -30,15 +43,18 @@ const Home = () => {
 				</div>
 				<div className='ms-2 p-3 bg-dark rounded d-flex'>
 					{digits.map((digit, index) => (
-				<div className='fs-2 ms-3 p-2 bg-secondary text-white rounded border border-danger' key={index}>
+					<div className='fs-2 ms-3 p-2 bg-secondary text-white rounded border border-danger' key={index}>
 			  		{digit}
 				</div>
 				))}
+				</div>
+			<div className='d-flex flex-column'>
+				<button type="button" onClick={resetCounter} className="btn btn-dark">Reset</button>
+				<button type="button" onClick={stopCounter} className="btn btn-dark">Stop</button>
+				<button type="button" onClick={resumeCounter} className="btn btn-dark">Continue</button>
 			</div>
 			</div>
 		  );
 		}
-
-	
 export default Home;
 
